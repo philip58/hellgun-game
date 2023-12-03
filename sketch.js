e9c2d1ec37d3bbb9eff734e36abc9866467d57a9
Philip Fernandez <philipfernandezz@gmail.com>
2023-12-02 22:31:43 -0500

    handle out of bounds and player death/restart

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@
 let player;
 let canDodge = true;
 let arm;
+
 //floor variables(s)
 let floor;
 
@@ -16,6 +17,24 @@ let projectiles = [];
 //camera variable(s)
 let shakeScreen = false;
 
+//game variables
+let playerIsDead = false;
+let showDeathScreen = false;
+
+//respawn the player at start point function
+function respawnPlayer(){
+    //delete floating objects if existing
+    if(projectiles.length!=0){
+        projectiles = [];
+    }
+
+    player.x = 256/7;
+    player.y = 200;
+
+    playerIsDead = false;
+    showDeathScreen = false;
+}
+
 //set up function
 function setup(){
     //set up canvas and world settings
@@ -23,7 +42,6 @@ function setup(){
     noStroke();
     rectMode(CENTER);
     world.gravity.y = 10;
-
     //initialize player
     player = new Sprite(0,0,256/7,200); //hitbox?
     player.collider = "dynamic";   
@@ -33,7 +51,7 @@ function setup(){
     
     //player arm
     arm = new Sprite(player.x,player.y,20,20);
-    arm.collider ="none";
+    arm.collider ="dynamic";
     arm.img = 'assets/arm.png';
     //player animations    
     player.addAni('idle', 'assets/idle.png',{frameSize: [256,256], frames: 30});
@@ -42,7 +60,6 @@ function setup(){
     //initialize floor
     floor = new Sprite(600,400,2000,300);
     floor.collider = "static";
-
 }
 
 function moveCamera(){
@@ -68,106 +85,134 @@ function draw(){
     // player.debug = mouse.pressing();
     // arm.debug = mouse.pressing();
 
-    //Player Arm
-
-    arm.x = player.x + 12;
-    arm.y = player.y - 17;
-
     //shake the screen
     //shake the camera when true
     if(shakeScreen){
         moveCamera();
     } else{
-        camera.x = player.x+600;
-        camera.y = player.y-250;
-    }
-    //adjust camera to follow player
-
-    player.rotation = 0;
-
-    //handling jumping 
-    if(player.colliding(floor) && kb.pressing('up')){
-        //jump only when touching floor
-        player.moveTo(player.x,player.y-200,8);
-    }
-
-    //handling movement
-    if (kb.pressing('left')) {
-        player.vel.x = -7;
-    } else if (kb.pressing('right')) {
-        player.vel.x = 7; 
-    } else {
-        player.vel.x = 0;
-    } 
-
-    //dodge left and right mechanic
-    if(canDodge && kb.presses('down') ) {
-        if(kb.pressing('left')){
-            player.x = player.x - 300;
-            console.log("dodge left");
-            canDodge = false;
+        if(player.y > 1080/2+200 || playerIsDead){
+            //handle player death
+            playerIsDead = true;
             setTimeout(() => {
-                canDodge = true;
+                //reset camera when player falls off level
+                camera.x = 600;
+                camera.y = -200;
+                showDeathScreen = true;
             }, 1000);
-        }
-        if(kb.pressing('right')){
-            player.x = player.x + 300;
-            console.log("dodge right");
-            canDodge = false;
-            setTimeout(() => {
-                canDodge = true;
-            }, 1000);
-        }
-
-    }  
-
-
-    //aiming
-    //flipping player and arm model based on mouse orientation
-    if (mouse.x > player.x){
-        player.mirror.x = false;
-        arm.mirror.x = false;
-    } else if (mouse.x < player.x){
-        player.mirror.x = true;
-        arm.mirror.x = true;
-        arm.x-=25;
-    }
-
-    //animations
-    player.anis.offset.y=-29;
-    player.anis.offset.x=10;
-
-
-    if (kb.pressing('left')) {
-		player.changeAni('walkForward');
-	} else if (kb.pressing('right')) {
-		player.changeAni('walkForward');
-	} else {
-    //play idle animation if movement stops
-		player.changeAni('idle');
-	}
-
-    //shooting wherever mouse is clicked
-    if(mouse.presses() && !player.mouse.hovering() && (mouse.x > player.x+80 || mouse.x < player.x-80)){
-        if (mouse.x > player.x){
-            projectile = new Sprite(player.x+80, player.y-30, 25);
+            if(showDeathScreen){
+                textSize(125);
+                text("You Have Died",500,150);
+                textSize(75);
+                text("Press R To Restart",600,250);
+            }   
+            if(kb.presses("r")){
+                respawnPlayer();
+            }
         } else{
-            projectile = new Sprite(player.x-80, player.y-30, 25);
-
+            //adjust camera to follow player
+            camera.x = player.x+600;
+            camera.y = player.y-250;
+            
         }
-        projectile.mass = 0;
-        projectiles.push(projectile);
-        projectile.moveTowards(mouse.x,mouse.y,75);
-        shakeTheScreen();
     }
 
-    //check for projectile collisions 
-    for(let i = 0; i < projectiles.length; i++){
-        if(projectiles[i].y<-650 || projectiles[i].y>=750){
-            projectiles[i].remove();
+    //Player Arm
+    arm.x = player.x + 12;
+    arm.y = player.y - 17;
+
+    //check if player is dead
+    if(!playerIsDead){
+        //handling jumping 
+        if(player.colliding(floor) && kb.pressing('up')){
+            //jump only when touching floor
+            player.moveTo(player.x,player.y-200,8);
+        }
+
+        //handling movement
+        if (kb.pressing('left')) {
+            player.vel.x = -7;
+        } else if (kb.pressing('right')) {
+            player.vel.x = 7; 
+        } else {
+            player.vel.x = 0;
         } 
-        if(projectiles[i].collides(floor)){
-            projectiles[i].remove();
+
+        //dodge left and right mechanic
+        if(canDodge && kb.presses('down') ) {
+            if(kb.pressing('left')){
+                player.x = player.x - 300;
+                console.log("dodge left");
+                canDodge = false;
+                setTimeout(() => {
+                    canDodge = true;
+                }, 1000);
+            }
+            if(kb.pressing('right')){
+                player.x = player.x + 300;
+                console.log("dodge right");
+                canDodge = false;
+                setTimeout(() => {
+                    canDodge = true;
+                }, 1000);
+            }
+
+        }  
+
+
+        //aiming
+        //flipping player and arm model based on mouse orientation
+        if (mouse.x > player.x){
+            player.mirror.x = false;
+            arm.mirror.x = false;
+            arm.rotation = mouse.y/75;
+        } else if (mouse.x < player.x){
+            player.mirror.x = true;
+            arm.mirror.x = true;
+            arm.x-=25;
+            arm.rotation = -mouse.y/75;
+        }
+
+        //animations
+        player.anis.offset.y=-29;
+        player.anis.offset.x=10;
+
+
+        if (kb.pressing('left')) {
+            player.changeAni('walkForward');
+        } else if (kb.pressing('right')) {
+            player.changeAni('walkForward');
+        } else {
+        //play idle animation if movement stops
+            player.changeAni('idle');
+        }
+
+        //shooting wherever mouse is clicked
+        if(mouse.presses() && !player.mouse.hovering() && ((mouse.x > player.x+80 || mouse.x < player.x-80) || (mouse.y<player.y-150))){
+            // if (mouse.x > player.x){
+            //     projectile = new Sprite(player.x+80, player.y-30, 25);
+            // } else{
+            //     projectile = new Sprite(player.x-80, player.y-30, 25);
+
+            // }
+            if (mouse.x > player.x){
+                projectile = new Sprite(arm.x+80,arm.y-10,25);
+            } else{
+                projectile = new Sprite(arm.x-80,arm.y-10,25);
+            }
+            projectile.mass = 0;
+            projectiles.push(projectile);
+            projectile.moveTowards(mouse.x,mouse.y,75);
+            shakeTheScreen();
+        }
+
+        //check for projectile collisions 
+        for(let i = 0; i < projectiles.length; i++){
+            if(projectiles[i].y<-650 || projectiles[i].y>=750){
+                projectiles[i].remove();
+            } 
+            if(projectiles[i].collides(floor)){
+                projectiles[i].remove();
+            }
         }
     }
 
